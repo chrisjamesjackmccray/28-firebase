aaa84590f900ec0fb77d0a7ef8523038b6172126
James McCray <chrisjamesjackmccray@gmail.com>
2016-04-14 14:42:33 -0500

    Assignment Commit

--- app/modules/todos/controller.js
+++ app/modules/todos/controller.js
@@ -1,7 +1,26 @@
 class FireTestController {
-  constructer() {
+  constructer($firebaseObject) {
 
-  }
+
+   this.list = $firebaseArray
+
+
+    this.random = "";
+
+    addToList() {
+      this.list.$add( {
+        name: this.item,
+        picked.false
+      });
+    }
+
+
+    togglePurchased(item) {
+      item.picked = !item.bought;
+      this.list.$save(item);
+    }
+
+  ]
 }
 
 export default FireTestController;
